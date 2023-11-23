bbadd4b92c8307d539349c555c270e411d148785
Aleksey Kononenko <a.kononenko@neovoxtech.ru>
2023-11-23 22:09:25 +0300

    - Подключил Laravel Breeze для использования авторизации и аутентификации пользователей

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
-use App\Http\Controllers\AppController;
-use App\Http\Controllers\CongratulationController;
-use App\Http\Controllers\EmployeeController;
-use App\Http\Controllers\MailTemplateController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +13,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-Route::get('/', [AppController::class, 'index'])->name('app.index');
 
-Route::resource('employees', EmployeeController::class);
-Route::resource('mail-templates', MailTemplateController::class);
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/send-congratulation/{employeeId}', function(string $employeeId) {
-    $congratulationController = new CongratulationController($employeeId);
-    $congratulationController->send();
-})->name('congratulations.send');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
