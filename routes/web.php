b115526ce9fb843499147c91f858bd7b7e431735
Aleksey <aleksey19988@gmail.com>
2023-12-27 09:12:46 +0300

    - Переместил route для dashboard в middleware

--- routes/web.php
+++ routes/web.php
@@ -33,13 +33,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    });
-
     Route::get('/', [AppController::class, 'index'])->name('app.index');
     Route::resource('employees', EmployeeController::class);
     Route::resource('positions', PositionController::class);
