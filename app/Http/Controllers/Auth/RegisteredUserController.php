3d48e7288ac491a58eac42480c42882fca83a61f
Aleksey <aleksey19988@gmail.com>
2023-12-28 19:02:08 +0300

    fix: Добавлено временное решение для ограничения доступа к регистрации

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -20,7 +20,10 @@ class RegisteredUserController extends Controller
      */
     public function create(): View
     {
-        return view('auth.register');
+        if (Auth::user()) {
+            return view('auth.register');
+        }
+        abort(403);
     }
 
     /**
