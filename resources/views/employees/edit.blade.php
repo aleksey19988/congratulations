7835c61b38fd9301cef38fc589689f7840fd332a
Aleksey <aleksey19988@gmail.com>
2023-12-31 14:29:11 +0300

    feat: update-interfaces - Создал новый компонент формы для выпадающего списка - Реализовал использование компонентов для форм добавления и редактирования сотрудника

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -21,68 +21,74 @@
                 <form action="{{ route('employees.update', $employee->id) }}" method="post" class="edit-employee-form flex flex-col">
                     @csrf
                     @method('PATCH')
-                    <x-text-input
-                        type="text"
-                        name="first_name"
-                        class="@error('first_name') border-2 border-rose-500 @enderror input-field m-3 w-96 p-3"
-                        placeholder="Имя"
-                        value="{{ $employee->first_name }}"
-                    ></x-text-input>
-                    @error('first_name')
-                    <div class="error-message text-rose-500 flex justify-center">{{ $message }}</div>
-                    @enderror
-                    <x-text-input
-                        type="text"
-                        name="last_name"
-                        class="@error('last_name') border-2 border-rose-500 @enderror input-field m-3 w-96 p-3"
-                        placeholder="Фамилия"
-                        value="{{ $employee->last_name }}"
-                    ></x-text-input>
-                    @error('last_name')
-                    <div class="error-message text-rose-500 flex justify-center">{{ $message }}</div>
-                    @enderror
-                    <x-text-input
-                        type="text"
-                        name="patronymic"
-                        class="@error('patronymic') border-2 border-rose-500 @enderror input-field m-3 w-96 p-3"
-                        placeholder="Отчество (при наличии)"
-                        value="{{ $employee->patronymic }}"
-                    ></x-text-input>
-                    @error('patronymic')
-                    <div class="error-message text-rose-500 flex justify-center">{{ $message }}</div>
-                    @enderror
-                    <x-text-input
-                        type="date"
-                        name="birthday"
-                        class="@error('birthday') border-2 border-rose-500 @enderror input-field m-3 w-96 p-3"
-                        placeholder="Дата рождения"
-                        value="{{ \Carbon\Carbon::make($employee->birthday)->format('Y-m-d') }}"
-                    ></x-text-input>
-                    @error('birthday')
-                    <div class="error-message text-rose-500 flex justify-center">{{ $message }}</div>
-                    @enderror
-                    <x-text-input
-                        type="email"
-                        name="email"
-                        class="@error('email') border-2 border-rose-500 @enderror input-field m-3 w-96 p-3"
-                        placeholder="Электропочта"
-                        value="{{ $employee->email }}"
-                    ></x-text-input>
-                    @error('email')
-                    <div class="error-message text-rose-500 flex justify-center">{{ $message }}</div>
-                    @enderror
-                    <div class="m-3">
-                        <select id="position_id" name="position_id"
-                                class="input-field w-96 h-12 p-3 block border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6">
-                            @php /** @var \App\Models\Position $position */ @endphp
-                            @foreach($positions as $position)
-                                <option
-                                    value="{{ $position->id }}"
-                                    {{ $employee->position->id === $position->id ? 'selected' : '' }}
-                                >{{ $position->name }}</option>
-                            @endforeach
-                        </select>
+
+                    <div class="mb-3 flex justify-center">
+                        <x-text-input
+                            type="text"
+                            name="first_name"
+                            class="w-96 p-3"
+                            placeholder="Имя"
+                            value="{{ $employee->first_name }}"
+                        ></x-text-input>
+                        <x-input-error :messages="$errors->get('first_name')" class="mt-2" />
                     </div>
+
+                    <div class="mb-3 flex justify-center">
+                        <x-text-input
+                            type="text"
+                            name="last_name"
+                            class="w-96 p-3"
+                            placeholder="Фамилия"
+                            value="{{ $employee->last_name }}"
+                        ></x-text-input>
+                        <x-input-error :messages="$errors->get('last_name')" class="mt-2" />
+                    </div>
+
+                    <div class="mb-3 flex justify-center">
+                        <x-text-input
+                            type="text"
+                            name="patronymic"
+                            class="w-96 p-3"
+                            placeholder="Отчество (при наличии)"
+                            value="{{ $employee->patronymic }}"
+                        ></x-text-input>
+                        <x-input-error :messages="$errors->get('patronymic')" class="mt-2" />
+                    </div>
+
+                    <div class="mb-3 flex justify-center">
+                        <x-text-input
+                            type="date"
+                            name="birthday"
+                            class="w-96 p-3"
+                            placeholder="Дата рождения"
+                            value="{{ \Carbon\Carbon::make($employee->birthday)->format('Y-m-d') }}"
+                        ></x-text-input>
+                        <x-input-error :messages="$errors->get('birthday')" class="mt-2" />
+                    </div>
+
+                    <div class="mb-3 flex justify-center">
+                        <x-text-input
+                            type="email"
+                            name="email"
+                            class="w-96 p-3"
+                            placeholder="Электропочта"
+                            value="{{ $employee->email }}"
+                        ></x-text-input>
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+
+                    <div class="mb-3 flex justify-center">
+                        <x-select-input
+                            class="w-96 p-3"
+                            name="position_id"
+                            :isUpdate="true"
+                            :options="$positions"
+                            :relatedModel="$employee"
+                            :propertyName="'position'"
+                        >
+                        </x-select-input>
+                    </div>
+
                     <button type="submit" class="save-edit-employee-form-button m-3 w-96 p-3"
                             id="form-button">Обновить
                     </button>
